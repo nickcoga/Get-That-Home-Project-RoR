8ed9ac31217da7375533c83844d69651628efa14
Nick Correa <nickcorreagarcia@gmail.com>
2021-03-29 16:43:26 -0500

    Created controllor Profile

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,34 @@
+class ProfileController < ApplicationController
+  def show
+    render json: current_user
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :bad_request
+    end
+  end
+
+  def update
+    if current_user.update(user_params)
+      render json: current_user
+    else
+      render json: current_user.errors, status: :bad_request
+    end
+  end
+
+  def destroy
+    current_user.destroy
+    render status: :no_content
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'profile/show'
+  get 'profile/create'
+  get 'profile/update'
+  get 'profile/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
