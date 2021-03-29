b41714719aac2b363a874964c114f39ab26124bb
Nick Correa <nickcorreagarcia@gmail.com>
2021-03-29 12:43:45 -0500

    Created model User with devise and config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.active_support.deprecation = :log
 
   # Raise an error on page load if there are pending migrations.
