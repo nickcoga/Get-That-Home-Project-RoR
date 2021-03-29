bdddc7bef65c16d7ffa47d9bcae1099a355c5eb8
Nick Correa <nickcorreagarcia@gmail.com>
2021-03-29 13:05:32 -0500

    Create model Photo and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_180015) do
+ActiveRecord::Schema.define(version: 2021_03_29_180414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.bigint "property_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["property_id"], name: "index_photos_on_property_id"
+  end
 
   create_table "properties", force: :cascade do |t|
     t.string "name"
@@ -55,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_03_29_180015) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "photos", "properties"
   add_foreign_key "property_users", "properties"
   add_foreign_key "property_users", "users"
 end
