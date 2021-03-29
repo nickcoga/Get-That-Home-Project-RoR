c695aecee3007be455872178df1a050ff6196a82
Nick Correa <nickcorreagarcia@gmail.com>
2021-03-29 13:29:57 -0500

    Add enum role has_many and validates in model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum role: { admin: 'landlord', user: 'user' }
+  has_many :properties, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true
+  validates :name, presence: true
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ }
+  validates :encrypted_password, length: { minimum: 6,
+                                           message: 'Min 6 letters' }
 end
