0f9859ed0361906dbb38c25e00d70f927be883d7
Nick Correa <nickcorreagarcia@gmail.com>
2021-03-29 13:02:28 -0500

    Created model PropertyUser and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_173013) do
+ActiveRecord::Schema.define(version: 2021_03_29_180015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,18 @@ ActiveRecord::Schema.define(version: 2021_03_29_173013) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "property_users", force: :cascade do |t|
+    t.boolean "favorite"
+    t.boolean "closed"
+    t.boolean "contacted"
+    t.bigint "user_id", null: false
+    t.bigint "property_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["property_id"], name: "index_property_users_on_property_id"
+    t.index ["user_id"], name: "index_property_users_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -43,4 +55,6 @@ ActiveRecord::Schema.define(version: 2021_03_29_173013) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "property_users", "properties"
+  add_foreign_key "property_users", "users"
 end
